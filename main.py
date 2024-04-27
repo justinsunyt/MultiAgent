7192ecdd50699163513e9085f93c18db5932a8ba
Justin Sun <justinsunyt@gmail.com>
2024-04-26 23:54:57 -0400

    update cors

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
